786ceaafb9dde0ec8d064be1965080beac7ede83
Sai Kumar <gaddamsaikumar12345@gmail.com>
2026-07-28 06:20:52 +0530

    clarity js addition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -143,6 +143,21 @@ export default async function RootLayout({
           src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=Siqp6J"
         />
 
+        {/* Microsoft Clarity Tracking Script */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wfqn8nk2x3");
+            `,
+          }}
+        />
+
         <Script
           id="splash-screen-logic"
           strategy="beforeInteractive"
